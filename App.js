9a1bc28e48bb04332cb249b64bae59f9cd13ad92
Jonathan Nicolas Maciel Costa <20211094040012@ifrn.local>
2022-12-26 20:29:41 -0300

    contador

--- App.js
+++ App.js
@@ -1,11 +1,51 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React,{useState} from 'react';
+import { StyleSheet, Text, Touchable, TouchableOpacity, View, TextInput } from 'react-native';
 
 export default function App() {
+  
+  function minus_count (count) {
+    if (count - 1 < 0){
+      return setCount(count)
+    }
+    else
+    {
+      return setCount(count-1)
+    }
+  }
+  function reset_count (count) {
+
+    return setCount(count=0)
+  }
+
+  function nome(nome) {
+    if (nome == 0){
+      return <Text> Reseted !!! </Text>
+    }
+    else
+      return None
+  }
+  const [count,setCount] = useState(0);
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+   
+      
+      <Text style={styles.tex}>Quantidade de pessoas</Text>
+      <Text style={styles.textzin}>{ count }</Text>
+
+      <TouchableOpacity style={styles.butaozinpush} onPress={ ()=> setCount(count+1)}> 
+        <text>+</text>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={styles.butaozinminus} onPress={ ()=> minus_count(count)}>
+        <text>-</text>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={styles.butaozinresert} onPress={ ()=> reset_count(count)}>
+        <text style={styles.butaozinresert_text}>Resetar</text>
+      </TouchableOpacity>
+
     </View>
   );
 }
@@ -13,8 +53,58 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    alignContent: 'center',
+    backgroundColor: 'purple',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  butaozinresert:
+  {
+    flex: 0,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: "black",
+    flexDirection: "row",
+    position: "absolute",
+    bottom: "0%",
+    height: "5%",
+    width: "100%",
+  },
+  butaozinresert_text:
+  {
+    color: "white",
+  },
+  textzin:
+  {
+    color: "white",
+    fontSize: "50px",
+    marginBottom: "25%",
+  },
+  tex:
+  {
+    color: "white",
+  },
+  butaozinpush:
+  {
+    backgroundColor: 'green',
+    color: 'black',
+
+    marginTop: "1%",
+    marginBottom: "2px",
+    paddingTop: '25px',
+    paddingRight: "71px",
+    paddingBottom: "25px",
+    paddingLeft: "75px",
+    borderRadius: "5px",
+  },
+  butaozinminus:
+  {
+    backgroundColor: 'red',
+    paddingTop: '25px',
+    paddingRight: "75px",
+    paddingBottom: "25px",
+    paddingLeft: "75px",
+    borderRadius: "5px",
+  }
+
 });
